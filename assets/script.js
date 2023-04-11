702e8830780577d83e70b8f27f1c0eac3a14ade7
TyGosley <tygosley@gmail.com>
2023-04-10 17:54:38 -0700

    added card carousel

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+// Get the card and image elements
+// const card = document.querySelector('.card');
+// const image = document.querySelector('#card-image');
+
+// // Replace the circle and loading bar with the image once it has loaded
+// image.onload = () => {
+//   card.querySelector('.circle').style.display = 'none';
+//   card.querySelector('.bar').style.display = 'none';
+//   image.style.display = 'block';
+// }
+
+// // Set the source of the image to your image URL
+// image.src = './assets/images/starkind.png';
