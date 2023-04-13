d59136dc95779418c0b3402d2a7d898d7c89f7ab
TyGosley <tygosley@gmail.com>
2023-04-13 12:40:39 -0700

    added images, added js file with onclick, added card carousel for projects.  Change CSS to make cards bigger and readable text

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,56 @@
-// Get the card and image elements
-// const card = document.querySelector('.card');
-// const image = document.querySelector('#card-image');
+// Card Carousel 
+const card1 = document.querySelector('#metalMindzCard');
+card1.addEventListener('click', function() {
+  window.open('https://metal-mindz.herokuapp.com/', '_blank');
+});
 
-// // Replace the circle and loading bar with the image once it has loaded
-// image.onload = () => {
-//   card.querySelector('.circle').style.display = 'none';
-//   card.querySelector('.bar').style.display = 'none';
-//   image.style.display = 'block';
-// }
+const card2 = document.querySelector('#filmFilterCard');
+card2.addEventListener('click', function() {
+  window.open('https://michaelx626.github.io/Film-Filter/index.html', '_blank');
+});
 
-// // Set the source of the image to your image URL
-// image.src = './assets/images/starkind.png';
+const card3 = document.querySelector('#cityWeatherMonitorCard');
+card3.addEventListener('click', function() {
+  window.open('https://tygosley.github.io/city-weather-monitor/', '_blank');
+});
+
+const card4 = document.querySelector('#nerdyByteBeatCard');
+card4.addEventListener('click', function() {
+  window.open('https://obscure-fortress-82547.herokuapp.com/', '_blank');
+});
+
+const card5 = document.querySelector('#digitalPencilPusherCard');
+card5.addEventListener('click', function() {
+  window.open('https://afternoon-springs-13154.herokuapp.com/', '_blank');
+});
+
+const card6 = document.querySelector('#teamProfileGenCard');
+card6.addEventListener('click', function() {
+  window.open('https://github.com/TyGosley/team-profile-generator', '_blank');
+});
+
+const card7 = document.querySelector('#codeQuizCard');
+card7.addEventListener('click', function() {
+  window.open('https://tygosley.github.io/code-quiz/', '_blank');
+});
+
+const card8 = document.querySelector('#passGenCard');
+card8.addEventListener('click', function() {
+  window.open('https://tygosley.github.io/password-generator-js/', '_blank');
+});
+
+const card9 = document.querySelector('#workforceTrackerCard');
+card9.addEventListener('click', function() {
+  window.open('https://github.com/TyGosley/workforce-tracker', '_blank');
+});
+
+const card10 = document.querySelector('#starkIndInternCard');
+card10.addEventListener('click', function() {
+  window.open('https://tygosley.github.io/stark-internship/', '_blank');
+});
+
+const card11 = document.querySelector('#workDaySchedCard');
+card11.addEventListener('click', function() {
+  window.open('https://tygosley.github.io/work-day-scheduler/', '_blank');
+});
+
